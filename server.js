1cf6969f3d9b4b3f4906df95dd0a441e69d31b10
Henrique <ricci.henrique@outlook.com>
2020-01-20 20:22:34 -0300

    Correção banco

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const parser = require('body-parser');
 
 const app = express();
 app.use(parser.urlencoded({extended:true}));
-const strCon = "postgres://mxyyzybl:qCNIIgJiY6qwEg0kZ-SARWv5BdpueQKU@rajje.db.elephantsql.com:5432/mxyyzybl";
+const strCon = "postgres://pzuxtdyftdnblj:3e15c181b02e1612fa3bb3ee622e0a4a27d7af867ba3ad8504d2e4c7c8e8d1c7@ec2-34-193-42-173.compute-1.amazonaws.com:5432/d6m6vhr2q7p6mc";
 
 var client;
 
@@ -17,7 +17,7 @@ app.get('/gerar', (req, res) => {
         
         if(query) {
             query.rows.forEach(row => {
-                res.send(row.des_desc + " ou compre " + row.cat_qtd + " cartas");
+                res.send(row.des_desc + " ou compre " + row.cat_qtde + " cartas");
             });
             client.end().then().catch(err => console.log(err));
         }
