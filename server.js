01b1d9575a838f4db8a19390582c2c623e466e68
Henrique <ricci.henrique@outlook.com>
2020-01-20 13:51:43 -0300

    d

--- server.js
+++ server.js
@@ -4,21 +4,22 @@ var pg = require('pg');
 const app = express();
 const strCon = "postgres://mxyyzybl:qCNIIgJiY6qwEg0kZ-SARWv5BdpueQKU@rajje.db.elephantsql.com:5432/mxyyzybl";
 
-var client = new pg.Client(strCon);
+var client/* = new pg.Client(strCon)*/;
 
 app.get('/gerar', (req, res) => {
-    client.connect(err => console.log("Erro: " + err));
+    client = new pg.Client(strCon);
+    client.connect().then(() => console.log("Conectado")).catch(err => console.log(err));
     client.query("select * from desafio d inner join categoria c on d.cat_cod = c.cat_cod order by RANDOM () LIMIT 1", (err, query) => {
         if(err)
-            console.log(err);
+            client.end().then(() => console.log("ok")).catch(err => console.log(err));
         
         if(query) {
             query.rows.forEach(row => {
                 res.send(row.des_desc + " ou compre " + row.cat_qtd + " cartas");
             });
+            client.end().then().catch(err => console.log(err));
         }
     });
-    client.end();
 });
 
 app.get('/', (req, res) => {    
