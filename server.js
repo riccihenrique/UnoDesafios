85f802df46cff66931311770381547f1b7e5c59a
Henrique <ricci.henrique@outlook.com>
2020-01-20 21:15:46 -0300

    Correção CSS

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const parser = require('body-parser');
 
 const app = express();
 app.use(parser.urlencoded({extended:true}));
-const strCon = "postgres://pzuxtdyftdnblj:3e15c181b02e1612fa3bb3ee622e0a4a27d7af867ba3ad8504d2e4c7c8e8d1c7@ec2-34-193-42-173.compute-1.amazonaws.com:5432/d6m6vhr2q7p6mc";
+const strCon = "postgres://mxyyzybl:qCNIIgJiY6qwEg0kZ-SARWv5BdpueQKU@rajje.db.elephantsql.com:5432/mxyyzybl";
+//const strCon = "postgres://pzuxtdyftdnblj:3e15c181b02e1612fa3bb3ee622e0a4a27d7af867ba3ad8504d2e4c7c8e8d1c7@ec2-34-193-42-173.compute-1.amazonaws.com:5432/d6m6vhr2q7p6mc";
 
 var client;
 
@@ -59,22 +60,6 @@ app.post('/gravar', (req, res) => {
     });
 });
 
-function getCat(cod) {
-    client = new pg.Client(strCon);
-    client.connect().then().catch(err => console.log(err));
-    client.query("select * from categoria where cat_cod = " + cod, (err, query) => {
-        if(err)
-        {
-            console.log(err);
-            client.end().then().catch(err => console.log(err));
-        }        
-        if(query) {
-            client.end().then().catch(err => console.log(err));
-            return query.rows[0];
-        }
-    });
-}
-
 app.get('/desafios', (req, res) => {
     client = new pg.Client(strCon);
     client.connect().then().catch(err => console.log(err));
@@ -84,7 +69,7 @@ app.get('/desafios', (req, res) => {
            str = "err";        
         if(query) {
             query.rows.forEach(row => {                
-                str += "<span> Desafio: " + row.des_desc + " Categoria: " + row.cat_cod + "</span><br>"; 
+                str += "<div class=\"desafios\"><span class=\"des\">Desafio: " + row.des_desc + "</span><br><span class=\"cat\">Categoria: " + row.cat_cod + "</span></div>"; 
             })
         }
         client.end().then().catch(err => console.log(err));
